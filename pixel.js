563cf25ea338f338552e91c2763a53643d599926
Ashutosh Nath Agarwal <ash7594@gmail.com>
2015-04-04 18:42:00 +0530

    alphabet created

--- pixel.js
+++ pixel.js
@@ -1,20 +1,51 @@
+var sprite = document.getElementById('sprite');
 var canvas = document.getElementById("canvas"),
 	ctx = canvas.getContext('2d');
-
+var canvas2 = document.getElementById("canvas2"),
+	ctx2 = canvas2.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+canvas2.width = window.innerWidth*3;
+canvas2.height = window.innerHeight*3;
 
 var wCx = canvas.width/2,
 	wCy = canvas.height/2;
 
+//////////
+var imgSize = Math.round(canvas.width*2);
+var img;
+
+function prel() {
+	ctx2.drawImage(sprite,0,0,imgSize,imgSize);
+}
+
+var vectors = [];
+var movesToAlphabet = 10;
+var finalPixelSize = 1;
+var drawAlphabet = false;
+var trackMoves = 0;
+var key = -1;
 //////////
 
 var pixelMin = canvas.width * 0.002,
 	pixelMax = canvas.width * 0.004;
 var velMax = 10,
 	velMin = 1;
+var offsetDist = canvas.width * 0.03;
 
 //////////
+
+document.addEventListener('keydown',function (event) {
+    key = event.keyCode;
+}); 
+
+function vector(x,y,mx,my,r) {
+	this.x = x;
+	this.y = y;
+	this.mx = mx;
+	this.my = my;
+	this.r = r;
+}
 
 function pixel (r) {
 	this.x = 0;
@@ -33,17 +64,18 @@ function pixel (r) {
 }
 
 var pixels = [];
-var np = 500;
+var np = 100;
+var tempnp = 0;
 
 function init () {
-	if(np > 0) {
+	if(tempnp < np) {
 		pixels.push( new pixel (Math.random() * (pixelMax - pixelMin) + pixelMin) );
-		np--;
+		tempnp++;
 	}
 	for(var i=0;i<pixels.length;i++) {
 		if(pixels[i].animate == false) {
 			if(pixels[i].cR < pixels[i].R) {
-				pixels[i].cR+=2;
+				pixels[i].cR+=3;
 			} else {
 				pixels[i].cR = pixels[i].R;
 				pixels[i].animate = true;
@@ -53,7 +85,7 @@ function init () {
 			pixels[i].cy = wCy + pixels[i].cR * Math.sin(pixels[i].deg * Math.PI/180);
 		} else {
 			pixels[i].x = (pixels[i].x + 2)%360; pixels[i].deg = (pixels[i].deg+0.7)%360;
-			pixels[i].y = 25 * Math.sin(pixels[i].x * Math.PI/180);
+			pixels[i].y = offsetDist * Math.sin(pixels[i].x * Math.PI/180);
 			pixels[i].cx = wCx + (pixels[i].cR + pixels[i].y) * Math.cos(pixels[i].deg * Math.PI/180);
 			pixels[i].cy = wCy + (pixels[i].cR + pixels[i].y) * Math.sin(pixels[i].deg * Math.PI/180);
 		}		
@@ -61,10 +93,72 @@ function init () {
 		if(pixels[i].cr < pixels[i].r) pixels[i].cr++;
 		else pixels[i].cr = pixels[i].r;
 	}
-	render();
+}
+
+function checkKeyPressed() {
+	if(key!=-1) {
+		alphabet(key);
+	}
+	key = -1;
+}
+
+function alphabet(key) {
+	var num = 0;
+	img = ctx2.getImageData((key%16)*parseInt(imgSize/16),parseInt(key/16)*parseInt(imgSize/16),parseInt(imgSize/16),parseInt(imgSize/16));
+	for(var i=0;i<img.data.length;i+=4) {
+		if(img.data[i] > 0 && img.data[i] < 100) {
+			num++;
+		}
+	}
+
+	//console.log(num);
+	var temp = parseInt(num/np);
+	console.log(num + " " + temp);
+
+	for(var i=0,j=0,k=0;j<np;i+=4) {
+		if(img.data[i] > 0 && img.data[i] < 100) {
+			if(k==0) {
+				var tx = wCx - parseInt(imgSize/16)/2 + (i/4)%parseInt(imgSize/16);
+				var ty = wCy - parseInt(imgSize/16)/2 + (i/4)/parseInt(imgSize/16);
+				var mx = (tx-pixels[j].cx)/movesToAlphabet;
+				var my = (ty-pixels[j].cy)/movesToAlphabet;
+				var r = (pixels[j].cr-finalPixelSize)/movesToAlphabet;
+				vectors.push(new vector(tx,ty,mx,my,r));
+				j++;
+			}
+			k = (k+1)%temp;
+		}
+	}
+	drawAlphabet = true;
+}
+
+function updateAlphabet() {
+	if(trackMoves<movesToAlphabet) {
+		for(var i=0;i<pixels.length;i++) {
+			pixels[i].cx += vectors[i].mx;
+            pixels[i].cy += vectors[i].my;
+			pixels[i].cr -= vectors[i].r; 
+		}
+		trackMoves++;
+	} else {
+		for(var i=0;i<vectors.length;i++) {
+            pixels[i].cx = vectors[i].x;
+            pixels[i].cy = vectors[i].y;
+			pixels[i].cr = finalPixelSize;
+        }
+	}
 }
 
 function render () {
+	//////
+	checkKeyPressed();
+
+	if(!drawAlphabet) {
+		init();
+	} else {
+		updateAlphabet();
+	}
+	//////
 	ctx.fillStyle = "rgba(0,0,0,0.2)";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 	for(var i=0;i<pixels.length;i++) {
@@ -76,4 +170,5 @@ function render () {
 	}
 }
 
-setInterval(init,17);
+setInterval(render,17);
+window.onload = prel;
